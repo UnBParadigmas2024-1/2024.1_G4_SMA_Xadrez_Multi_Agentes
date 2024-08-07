d361fe124da25f7e671fa807b0e844eb31794888
200030264-Guilherme.Nishimura <78215376+Guilherme-Nishi@users.noreply.github.com>
2024-08-07 11:22:47 -0300

    Adicionando logica de ia no arquivo chess_ai

---
+++ chess_ai.py
@@ -0,0 +1,26 @@
+import chess
+import chess.engine
+import os
+
+class ChessAI:
+    def __init__(self):
+        self.stockfish_path = self.get_stockfish_path()
+        self.engine = chess.engine.SimpleEngine.popen_uci(self.stockfish_path)
+
+    def get_stockfish_path(self):
+        system = os.name
+        if system == 'nt':
+            return "./stockfish_windows/stockfish-windows-x86-64.exe"
+        elif os.uname().sysname == 'Darwin':
+            return "./stockfish_mac/stockfish-macos-x86-64"
+        elif system == 'posix':
+            return "./stockfish_ubuntu/stockfish-ubuntu-x86-64"
+        else:
+            raise Exception("Sistema operacional não suportado")
+
+    def get_move(self, board):
+        result = self.engine.play(board, chess.engine.Limit(time=2.0))
+        return result.move
+
+    def close(self):
+        self.engine.quit()
